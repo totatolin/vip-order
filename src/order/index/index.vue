1407783ef6125cb9580af64199a5e85cb5c4c5be
linsen <linsen@123feng.com>
2017-03-06 11:19:55 +0800

    环境配置

---
+++ src/order/index/index.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  .index-page
+    .index-wrapper
+      .index-wrapper-header.wfull.h25
+        img.pull-right(src="./images/sprites/logo-text.png")
+</template>
+
+<style lang="scss" scoped>
+  @import './index.scss';
+</style>
